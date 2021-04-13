69b097002541c25fb728d9a0721fe9f765f9f469
AlexeyKozlovsky <lexxxa142@gmail.com>
2021-04-13 14:23:33 +0300

    edited

--- Practise/Modules/segmentator.py
+++ Practise/Modules/segmentator.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import subprocess
 import json
 import math
@@ -9,12 +10,19 @@ import librosa
 import pytube as pt
 import numpy as np
 import pandas as pd
+import moviepy.editor as mp
 
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 
+sys.path.insert(1, '../Modules/')
+from chunck import Chunck
+
+
 class Segmentator:
 	"""Класс для сегментации видоефайлов"""
-	def __init__(self, conf):
+	def __init__(self, audio_model_path, landmarks_model_path, conf=1):
+		self.landmarks_model_path = landmarks_model_path
+		self.audio_model_path = audio_model_path
 		self.conf = conf # Вероятность, начиная с которой надо оставлять распознанные слов
 		self.word_dict = None
 		
@@ -26,9 +34,37 @@ class Segmentator:
 		path: путь для видео
 		out_path: путь для датасета
 		"""
-		
+		if self.word_dict is None:
+			self.word_dict = self.segment_words(path)
 
-		pass
+		for key in self.word_dict:
+			for index, word in enumerate(self.word_dict[key]):
+				max_index = 0
+
+				if word[2] >= self.conf:
+					l_path = f'{out_path}/{word[0]}'
+					w_path = f'{out_path}/{word[0]}/{word}'
+
+					if not os.path.exist(l_path):
+						os.mkdir(l_path)
+					if not os.path.exist(w_path):
+						os.mkdir(w_path)
+					else:
+
+						files = os.listdir()
+						for file in files:
+							max_index = max(max_index, file.partition('.'))
+
+					temp_path = f'{w_path}/{max_index}t.mp4'
+					ffmpeg_extract_subclip(path, word[0], word[1], temp_path)
+
+					ch = Chunck(path, self.landmarks_model_path)
+					ch.prepare()
+					ch.to_file(f'{w_path}/{max_index}.mp4')
+
+					if os.path.isfile(temp_path):
+						os.remove(temp_path)
+						
 
 	def _extract_words(self, res):
 		jres = json.loads(res)
@@ -54,26 +90,29 @@ class Segmentator:
 
 		return result
 
-	def to_cvs(self, model_path, input_path, out_path=None):
+	def segment_words(self, input_path, out_path=None):
 		"""
 		Метод для сбора информации о таймингах слов в
 		словарь или cvs файл
 
-		model_path: путь до модели для распознавания речи
 		input_path: путь к видеофайлу
 		out_path: путь к csv файлу. Если None, то сохранять не надо
 
 		returns: словарь слов с таймингами и вероятностями
 		"""
 
+		clip = mp.VideoFileClip(input_path)
+		temp_audio_path = 'audio.mp3'
+		clip.audio.write_audiofile(temp_audio_path)
+
 		vosk.SetLogLevel(-1)
 
 		sample_rate = 16000
-		audio, sr = librosa.load(input_path, sr=sample_rate)
+		audio, sr = librosa.load(temp_audio_path, sr=sample_rate)
 
 		int16 = np.int16(audio * 32768).tobytes()
 
-		model = vosk.Model(model_path)
+		model = vosk.Model(self.audio_model_path)
 		recognizer = vosk.KaldiRecognizer(model, sample_rate)
 
 		res = transcribe_words(recognizer, int16)
@@ -91,13 +130,7 @@ class Segmentator:
 		for index, row in df.iterrows():
 			word_dict[row['word']].append((row['start'], row['end'], row['conf']))
 
+		if os.path.isfile(temp_audio_path):
+			os.delete(temp_audio_path)
+
 		return word_dict
-
-
-
-
-
-
-
-			
-
